715180aede4c530afc1e186010408493a79ed01e
ardyes <ross.skipworth@outlook.com>
2019-12-06 15:17:47 +1100

    users section finished

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -9,6 +9,7 @@ use App\User;
 use App\Role;
 use App\Photo;
 use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -129,6 +130,10 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
-        return view('admin.users.destroy');
+        $user = User::findOrFail($id);
+        unlink(public_path() . $user->photo->file);
+        $user->delete();
+        Session::flash('deleted_user', 'The users has been deleted');
+        return redirect('/admin/users');
     }
 }
