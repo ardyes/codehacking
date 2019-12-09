d13cd0a243a65ed940e0b606cb9c3bfd8fd0c45d
ardyes <ross.skipworth@outlook.com>
2019-12-09 20:12:26 +1100

    able to create create posts with static category

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Post;
+use App\Http\Requests\PostsCreateRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Photo;
 
 class AdminPostsController extends Controller
 {
@@ -16,6 +20,8 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -26,6 +32,7 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
+        return view('admin.posts.create');
     }
 
     /**
@@ -34,9 +41,21 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+        $input = $request->all();
+        $user = Auth::user();
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+            $file->move('images', $name);
+            $photo = Photo::create(['file'=>$name]);
+            $input['photo_id'] = $photo->id;
+        }
+
+        $user->posts()->create($input);
+        return redirect('/admin/posts');
+
     }
 
     /**
